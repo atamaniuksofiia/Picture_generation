d1e6e1318d0fb2655115281f258a5c86b35b787e
atamaniuksofiia <165192279+atamaniuksofiia@users.noreply.github.com>
2024-10-01 20:27:29 +0300

    updated code

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'https://pixabay.com/api/';
+export const API_KEY = '46237249-998f0f24dffb15788a2eb0e4e';
+
+
+
+
---
+++ src/main.js
@@ -0,0 +1,62 @@
+import { fetchImages, createImageMarkup } from './js/render-functions';
+import { BASE_URL, API_KEY } from './js/pixabay-api.js';
+import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const form = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.js-load-more');
+
+let query = '';
+let page = 1;
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    query = document.querySelector('input[name="query"]').value.trim();
+    if (!query) {
+        iziToast.error({
+            title: 'Error',
+            message: 'Search query cannot be empty.',
+        });
+        return;
+    }
+
+    gallery.innerHTML = '';
+    page = 1;
+    loadMoreBtn.classList.add('is-hidden');
+
+    try {
+        const images = await fetchImages(query, page);
+        const markup = createImageMarkup(images);
+        gallery.innerHTML = markup;
+        loadMoreBtn.classList.remove('is-hidden');
+    } catch (error) {
+        console.error(error);
+        iziToast.error({
+            title: 'Error',
+            message: 'An error occurred while fetching images. Please try again later.',
+        });
+    }
+});
+
+loadMoreBtn.addEventListener('click', async () => {
+    page += 1;
+    try {
+        const images = await fetchImages(query, page);
+        const markup = createImageMarkup(images);
+        gallery.insertAdjacentHTML('beforeend', markup);
+        lightbox.refresh();
+    } catch (error) {
+        console.error(error);
+        iziToast.error({
+            title: 'Error',
+            message: 'An error occurred while fetching images. Please try again later.',
+        });
+    }
+});
