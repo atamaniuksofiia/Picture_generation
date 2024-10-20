370090689c5d2dcef321c5152f16fba37b4ed8be
atamaniuksofiia <165192279+atamaniuksofiia@users.noreply.github.com>
2024-10-20 12:31:54 +0300

    css-updating

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,40 +3,14 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import { BASE_URL, API_KEY } from './pixabay-api.js';
 
-// export async function fetchImages(query, page = 1) {
-//     const params = new URLSearchParams({
-//         key: API_KEY,
-//         q: query,
-//         image_type: 'photo',
-//         orientation: 'horizontal',
-//         safesearch: 'true',
-//         page: page,
-//         per_page: 15,
-//     });
-//     const url = `${BASE_URL}?${params}`;
-
-//     try {
-//         const response = await axios.get(url);
-//         if (response.status !== 200) {
-//             throw new Error('Network response was not ok');
-//         }
-//         return response.data.hits;
-//     } catch (error) {
-//         iziToast.error({
-//             title: 'Error',
-//             message: "Sorry, there are no images matching your search query. Please try again!",
-//         });
-//         return [];
-//     }
-// }
-
 export function createImageMarkup(images) {
     const markup = images
         .map(
             ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+            <div class="img-info-wrapper">
                 <li class="gallery-item">
                     <a href="${largeImageURL}" class="gallery-link">
-                        <img src="${webformatURL}" alt="${tags}" class="gallery-image" />
+                        <img src="${webformatURL}" alt="${tags}" class="gallery-image width="360" " />
                     </a>
                     <div class="info">
                         <p class="info-item"><span>Likes:</span> ${likes}</p>
@@ -45,6 +19,7 @@ export function createImageMarkup(images) {
                         <p class="info-item"><span>Downloads:</span> ${downloads}</p>
                     </div>
                 </li>
+                </div>
             `)
         .join('');
 
